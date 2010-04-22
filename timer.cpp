191c717b05e1a0ec3549bfaecf2888aca1244543
FlipperKid <FlipperKid@292f89c4-aa38-998b-c2c6-b1ae36ae8363>
2010-04-22 11:05:41 +0000

    added timer

---
+++ timer.cpp
@@ -0,0 +1,31 @@
+#include "timer.h"
+
+double secs = 0.0f;
+time_t start;
+bool running = false;
+
+void reset_timer( ) { 
+    running = false;
+    start= time(NULL);
+    secs = 0.0f; 
+}
+
+void start_timer( ) { 
+    start = time(NULL); 
+    running = true;
+}
+
+void stop_timer( ) {
+    if( running ) {
+        running = false;
+        secs += difftime(time(NULL),start);
+    }
+}
+
+double get_time_elapsed( ) {
+    if( running ) {
+        return secs + difftime(time(NULL), start);
+    }
+    return secs;
+}
+
---
+++ timer.h
@@ -0,0 +1,14 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void reset_timer( );
+void start_timer( );
+void stop_timer( );
+double get_time_elapsed( );
+
+#endif
+
