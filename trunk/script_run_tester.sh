48b44696122e5de440f12e9416d4f0e5c43b2d59
Raymond.W.Ko <Raymond.W.Ko@292f89c4-aa38-998b-c2c6-b1ae36ae8363>
2010-04-30 10:58:35 +0000

    Blah

--- trunk/script_run_tester.sh
+++ trunk/script_run_tester.sh
@@ -1,10 +1,9 @@
-<<<<<<< .mine
 #!/bin/bash
 
 EMAIL=wko@seas.upenn.edu
 
-#qsub -m a -M $EMAIL -cwd -b y -j y -q core2-quad.q -pe threaded 4 ./script_test.sh 50 65536 4 1 serial
-#qsub -m a -M $EMAIL -cwd -b y -j y -q core2-quad.q -pe threaded 4 ./script_test.sh 50 65536 4 1 openmp
+qsub -m a -M $EMAIL -cwd -b y -j y -q core2-quad.q -pe threaded 4 ./script_test.sh 50 65536 4 1 serial
+qsub -m a -M $EMAIL -cwd -b y -j y -q core2-quad.q -pe threaded 4 ./script_test.sh 50 65536 4 1 openmp
 
 for (( ii = 1; ii <= 1048576; ii = (ii * 2) ))
 do
